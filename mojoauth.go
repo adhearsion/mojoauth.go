6d9eba461d998f25bf98a8cbeaa2a55d935a0ada
Ben Langfeld <ben@langfeld.me>
2014-09-26 20:39:11 -0300

    Create random secrets

--- mojoauth.go
+++ mojoauth.go
@@ -1,1 +1,7 @@
 package mojoauth
+
+import "github.com/dchest/uniuri"
+
+func CreateSecret() string {
+  return uniuri.NewLen(93)
+}
--- mojoauth_test.go
+++ mojoauth_test.go
@@ -1,1 +1,11 @@
 package mojoauth
+
+import "testing"
+import "unicode/utf8"
+
+func TestCreateSecret(t *testing.T) {
+  const length = 93
+  if secret := CreateSecret(); utf8.RuneCountInString(secret) != length {
+    t.Errorf("CreateSecret created a string that wasn't %v characters", length)
+  }
+}
