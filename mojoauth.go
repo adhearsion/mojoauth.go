203d4a3e601415d904ee4da5e528c07f8d41f617
Luca Pradovera <luca.pradovera@gmail.com>
2014-10-12 15:31:47 +0200

    partial implementation

--- mojoauth.go
+++ mojoauth.go
@@ -1,7 +1,71 @@
 package mojoauth
 
-import "github.com/dchest/uniuri"
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"github.com/dchest/uniuri"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const dayInSeconds = 86400
 
 func CreateSecret() string {
-  return uniuri.NewLen(93)
+	return uniuri.NewLen(93)
+}
+
+func CreateCredentials(id string, secret string, ttl_seconds ...int) (username string, password string) {
+	ttl := dayInSeconds
+	if len(ttl_seconds) > 0 {
+		ttl = ttl_seconds[0]
+	}
+
+	total_ttl := nowTimeInt() + ttl
+
+	if id == "" {
+		username = fmt.Sprintf("%d", total_ttl)
+	} else {
+		username = fmt.Sprintf("%d:%s", total_ttl, id)
+	}
+
+	password = SignMessage(username, secret)
+	return
+}
+
+func SignMessage(message string, secret string) (signed string) {
+	key := []byte(secret)
+	h := hmac.New(sha1.New, key)
+	h.Write([]byte(message))
+	signed = base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return
+}
+
+func TestCredentials(username string, password string, secret string, ttl_timestamp ...int) (result bool, id string) {
+	result = true
+	id = ""
+	max_ttl := nowTimeInt()
+	split_parts := strings.Split(username, ":")
+	if len(split_parts) > 1 {
+		id = split_parts[1]
+	}
+
+	if len(ttl_timestamp) > 0 {
+		max_ttl = ttl_timestamp[0]
+	}
+
+	expiry_timestamp, _ := strconv.Atoi(split_parts[0])
+	if expiry_timestamp > max_ttl {
+		result = false
+	}
+	if SignMessage(username, secret) != password {
+		result = false
+	}
+	return
+}
+
+func nowTimeInt() int {
+	return int(time.Now().UTC().Unix())
 }
--- mojoauth_test.go
+++ mojoauth_test.go
@@ -1,11 +1,62 @@
 package mojoauth
 
-import "testing"
-import "unicode/utf8"
+import (
+	"fmt"
+	"testing"
+	"time"
+	"unicode/utf8"
+)
 
 func TestCreateSecret(t *testing.T) {
-  const length = 93
-  if secret := CreateSecret(); utf8.RuneCountInString(secret) != length {
-    t.Errorf("CreateSecret created a string that wasn't %v characters", length)
-  }
+	const length = 93
+	if secret := CreateSecret(); utf8.RuneCountInString(secret) != length {
+		t.Errorf("CreateSecret created a string that wasn't %v characters", length)
+	}
+}
+
+func TestCorrectCredentialsWithId(t *testing.T) {
+	const user_id = "mojo"
+	secret := CreateSecret()
+	username, password := CreateCredentials(user_id, secret)
+	result, id := TestCredentials(username, password, secret)
+	if result == false {
+		t.Errorf("Credentials should have been valid")
+	}
+	if id != user_id {
+		t.Errorf("ID should have been %s, instead it was %s", user_id, id)
+	}
+}
+
+func TestIncorrectCredentialsWithId(t *testing.T) {
+	const user_id = "mojo"
+	secret := CreateSecret()
+	username, password := CreateCredentials(user_id, secret)
+
+	result, _ := TestCredentials(username, "password", secret)
+	if result == true {
+		t.Errorf("Credentials should have been invalid with a wrong password")
+	}
+
+	result, _ = TestCredentials("username", password, secret)
+	if result == true {
+		t.Errorf("Credentials should have been invalid with a wrong username")
+	}
+
+	result, _ = TestCredentials(username, password, "this is wrong")
+	if result == true {
+		t.Errorf("Credentials should have been invalid with a different secret")
+	}
+
+	new_timestamp := time.Date(2020, time.October, 10, 23, 0, 0, 0, time.UTC)
+	forged_username := fmt.Sprintf("%d:%s", new_timestamp.Unix(), user_id)
+	result, _ = TestCredentials(forged_username, password, secret)
+	if result == true {
+		t.Errorf("Credentials should have been invalid with an username with a forged TTL")
+	}
+
+	old_timestamp := int(time.Date(2010, time.October, 10, 23, 0, 0, 0, time.UTC).Unix())
+	result, _ = TestCredentials(forged_username, password, secret, old_timestamp)
+	if result == true {
+		t.Errorf("Credentials should have been invalid with a timestamp that is expired")
+	}
 }
